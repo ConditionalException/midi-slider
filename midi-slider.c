5a9ed615c523ebe5f4da4e159f309bda928641db
Kyle Groleau <kyle.groleau@outlook.com>
2024-05-17 16:37:23 -0400

    Support 3 channels - Nothing fancy, if performance is an issue I'll look at using the DMA

--- midi-slider.c
+++ midi-slider.c
@@ -18,10 +18,21 @@ const uint8_t CLAMP_RANGE = 2;
 const uint8_t MIDI_MIN = 0;
 const uint8_t MIDI_MAX = 127;
 
-void update_midi_control(uint8_t);
+typedef struct midi_control_t {
+  uint8_t value;
+  uint8_t channel;
+  uint32_t last_update;
+} midi_control_t;
+
+void update_midi_control(midi_control_t *, uint8_t);
 
 int main() {
-  uint16_t val = 0;
+  uint16_t val[3] = {0};
+  midi_control_t controls[] = {
+      {.value = 0, .channel = 1, .last_update = 0},
+      {.value = 0, .channel = 2, .last_update = 0},
+      {.value = 0, .channel = 3, .last_update = 0},
+  };
   const float conversion_factor = (float)(MIDI_MAX + 1) / (1 << 12);
 
   stdio_init_all();
@@ -30,17 +41,22 @@ int main() {
   adc_init();
 
   adc_gpio_init(26);
+  adc_gpio_init(27);
+  adc_gpio_init(28);
+
+  adc_set_clkdiv(0);
   adc_select_input(0);
 
   while (1) {
     tud_task();
-
-    val = adc_read();
-    for (uint8_t i = 0; i < NUM_SAMPLES; i++) {
-      val = (val + adc_read()) / 2;
+    for (int i = 0; i < 3; i++) {
+      adc_select_input(i);
+      val[i] = adc_read();
+      for (uint8_t c = 0; c < NUM_SAMPLES; c++) {
+        val[i] = (val[i] + adc_read()) / 2;
+      }
+      update_midi_control(&controls[i], val[i] * conversion_factor);
     }
-
-    update_midi_control(val * conversion_factor);
   }
 }
 
@@ -66,10 +82,9 @@ void tud_resume_cb(void) { board_led_on; }
 // MIDI Task
 //--------------------------------------------------------------------+
 
-void update_midi_control(uint8_t val) {
+void update_midi_control(midi_control_t *control, uint8_t val) {
   uint8_t msg[3];
-  static uint8_t last_val = MIDI_MIN;
-  static uint32_t last_update = 0;
+  uint8_t difference = 0;
 
   // Guard from values above MIDI_MAX
   val = val % (MIDI_MAX + 1);
@@ -81,16 +96,15 @@ void update_midi_control(uint8_t val) {
   if (val <= (MIDI_MIN + CLAMP_RANGE)) {
     val = MIDI_MIN;
   }
+  difference = ((control->value > val) ? (control->value - val) : (val - control->value));
 
-  if (((last_val > val) ? (last_val - val) : (val - last_val)) >= DEVIATION &&
-      board_millis() - last_update >= UPDATE_RATE_MS) {
-    last_update = board_millis();
+  if ((difference >= DEVIATION) && (board_millis() - control->last_update) >= UPDATE_RATE_MS) {
+    control->last_update = board_millis();
+    control->value = val;
 
-    msg[0] = 176; // Control change
-    msg[1] = 1;   // Control
-    msg[2] = val; // Control val
+    msg[0] = 176; // Control type
+    msg[1] = control->channel;
+    msg[2] = val;
     tud_midi_n_stream_write(0, 0, msg, 3);
-
-    last_val = val;
   }
 }
